91149ee32d55917d2b135e43a6d622b000aec01a
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-09-23 19:48:47 +0600

    calculate function create and applied to all

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ function getElementNumById(id) {
 // Error Alert
 function errorAlert(id, errorText, btn) {
     const value = getInputNumById(id);
-    if (isNaN(value) || value < 0 || value > money) {
+    const money = getElementNumById("money")
+    if (isNaN(value) || value <= 0 || value > money) {
         document.getElementById(errorText).classList.remove("hidden");
         document.getElementById(btn).setAttribute("disabled", true);
     } else {
@@ -30,41 +31,113 @@ function btnToggle(mainBtn, shiftBtn) {
 
 
 // Main Functionalities 
-let money = getElementNumById("money");
-let totalDonate = getElementNumById("total-donate");
+// let money = getElementNumById("money");
+// let totalDonate = getElementNumById("total-donate");
 
 document.getElementById("noakhali-btn").addEventListener("click", () => {
-    const inputValue = getInputNumById("noakhali-input");
-    
-    if (isNaN(inputValue)) {
-        alert("Please Enter amount in input field");
-        return
-    } 
-    money -= inputValue;
+    // const inputValue = getInputNumById("noakhali-input");
 
-    totalDonate += inputValue;
-    document.getElementById("total-donate").innerHTML = totalDonate;
+    // if (isNaN(inputValue)) {
+    //     alert("Please Enter amount in input field");
+    //     return
+    // } 
+    // money -= inputValue;
 
-    document.getElementById("money").innerText = money;
-    document.getElementById("noakhali-input").value = "";
+    // totalDonate += inputValue;
+    // document.getElementById("total-donate").innerHTML = totalDonate;
 
-    const createEl = document.createElement("div");
-    createEl.className = "box space-y-4";
-    createEl.innerHTML = `
-                <h2 class="title">${inputValue} Taka is Donated for ${document.getElementById("noakhali-title").innerHTML}</h2>
-                <p class="description">Date : ${new Date().toLocaleString()}</p>
-    `
-    document.getElementById("history-container").append(createEl);
+    // document.getElementById("money").innerText = money;
+    // document.getElementById("noakhali-input").value = "";
 
+    // const createEl = document.createElement("div");
+    // createEl.className = "box space-y-4";
+    // createEl.innerHTML = `
+    //             <h2 class="title">${inputValue} Taka is Donated for ${document.getElementById("noakhali-title").innerHTML}</h2>
+    //             <p class="description">Date : ${new Date().toLocaleString()}</p>
+    // `
+    // document.getElementById("history-container").append(createEl);
+
+    calculateDonation("noakhali-input", "money", "noakhali-donate", "noakhali-title");
 });
-
-
-
-
 
 document.getElementById("noakhali-input").addEventListener("input", () => {
     errorAlert("noakhali-input", "noakhali-error", "noakhali-btn");
 });
+
+// Feni Function
+document.getElementById("feni-btn").addEventListener("click", () => {
+    calculateDonation("feni-input", "money", "feni-donate", "feni-title");
+
+});
+document.getElementById("feni-input").addEventListener("input", () => {
+    errorAlert("feni-input", "feni-error", "feni-btn", "money");
+});
+
+
+// Quota Function
+document.getElementById("quota-btn").addEventListener("click", () => {
+    calculateDonation("quota-input", "money", "quota-donate", "quota-title");
+
+});
+document.getElementById("quota-input").addEventListener("input", () => {
+    errorAlert("quota-input", "quota-error", "quota-btn", "money");
+});
+
+
+// Calculate Function
+function calculateDonation(id, money, donate, title) {
+    let balance = getElementNumById(money);
+    let donateBalance = getElementNumById(donate)
+    
+    const inputValue = getInputNumById(id);
+    if (isNaN(inputValue)) {
+        document.getElementById("my_modal_1").close();
+        alert("Please enter a Valid Amount");
+        return
+    } else {
+        balance -= inputValue;
+
+        document.getElementById(donate).innerHTML = donateBalance + inputValue;
+        document.getElementById(money).innerHTML = balance;
+
+        document.getElementById(id).value = "";
+
+        const createEl = document.createElement("div");
+        createEl.className = "box space-y-4";
+        createEl.innerHTML = `
+        <h2 class="title">${inputValue} Taka is Donated for ${document.getElementById(title).innerHTML}</h2> 
+        <p class="description">Date : ${new Date()}</p>
+        `
+        
+        document.getElementById("history-container").append(createEl);
+    }
+    
+    
+    console.log(count);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.getElementById("history-tab").addEventListener("click", () => {
     btnToggle("history-tab", "donation-tab")
