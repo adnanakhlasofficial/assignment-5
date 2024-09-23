a09e5f928b683dbc7598b789305433d140f7c4c8
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-09-23 10:43:23 +0600

    Tab Shifting, Error Alert Function Added & History Created

--- src/index.js
+++ src/index.js
@@ -1,40 +1,75 @@
 // Utilities Function
+// Get Input Value Number
 function getInputNumById(id) {
-    return Number(document.getElementById(id).value);
+    return parseFloat(document.getElementById(id).value);
+};
+// Get Element Text
+function getElementNumById(id) {
+    return parseFloat(document.getElementById(id).innerText);
 };
 
-function getElementNumById(id) {
-    return Number(document.getElementById(id).innerText);
+// Error Alert
+function errorAlert(id, errorText, btn) {
+    const value = getInputNumById(id);
+    if (isNaN(value) || value < 0 || value > money) {
+        document.getElementById(errorText).classList.remove("hidden");
+        document.getElementById(btn).setAttribute("disabled", true);
+    } else {
+        document.getElementById(errorText).classList.add("hidden");
+        document.getElementById(btn).removeAttribute("disabled");
+    }
 };
+
+// Button Toggle
+function btnToggle(mainBtn, shiftBtn) {
+    document.getElementById(shiftBtn).classList.remove("btn-main");
+    document.getElementById(shiftBtn).classList.add("btn-secondary");
+    document.getElementById(mainBtn).classList.add("btn-main");
+    document.getElementById(mainBtn).classList.remove("btn-secondary");
+}
 
 
 // Main Functionalities 
 let money = getElementNumById("money");
 let totalDonate = getElementNumById("total-donate");
-document.getElementById("noakhali-btn").addEventListener("click", ()=> {
+
+document.getElementById("noakhali-btn").addEventListener("click", () => {
     const inputValue = getInputNumById("noakhali-input");
     money -= inputValue;
-    
+
     totalDonate += inputValue;
     document.getElementById("total-donate").innerHTML = totalDonate;
 
     document.getElementById("money").innerText = money;
     document.getElementById("noakhali-input").value = "";
-    
+
+    const createEl = document.createElement("div");
+    createEl.className = "box space-y-4";
+    createEl.innerHTML = `
+                <h2 class="title">${inputValue} Taka is Donated for ${document.getElementById("noakhali-title").innerHTML}</h2>
+                <p class="description">Date : ${new Date().toLocaleString()}</p>
+    `
+    document.getElementById("history-container").append(createEl);
+
 });
 
+
+
+
+
 document.getElementById("noakhali-input").addEventListener("input", () => {
-    const value =  getInputNumById("noakhali-input");
-    if (isNaN(value) ||  value < 0 || value > money) {
-        const errorMsg = document.getElementById("noakhali-error");
-        errorMsg.classList.remove("hidden");
-        const button = document.getElementById("noakhali-btn");
-        button.setAttribute("disabled", true);
-    } else {
-        const errorMsg = document.getElementById("noakhali-error");
-        errorMsg.classList.add("hidden");
-        const button = document.getElementById("noakhali-btn");
-        button.removeAttribute("disabled");
-    }
-    
-})
+    errorAlert("noakhali-input", "noakhali-error", "noakhali-btn");
+});
+
+document.getElementById("history-tab").addEventListener("click", () => {
+    btnToggle("history-tab", "donation-tab")
+    document.getElementById("history-container").classList.remove("hidden");
+    document.getElementById("donation-container").classList.add("hidden");
+});
+document.getElementById("donation-tab").addEventListener("click", () => {
+    btnToggle("donation-tab", "history-tab")
+    document.getElementById("donation-container").classList.remove("hidden");
+    document.getElementById("history-container").classList.add("hidden");
+});
+
+
