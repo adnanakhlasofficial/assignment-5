df7a838d1c3ea7455f71e06528fc8d0dd401b5ed
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-09-23 08:51:38 +0600

    Noakhali Donate proper Fucntion added

--- src/index.js
+++ src/index.js
@@ -10,16 +10,15 @@ function getElementNumById(id) {
 
 // Main Functionalities 
 let money = getElementNumById("money");
+let totalDonate = getElementNumById("total-donate");
 document.getElementById("noakhali-btn").addEventListener("click", ()=> {
     const inputValue = getInputNumById("noakhali-input");
-    const donate = money - inputValue
-    console.log(donate);
+    money -= inputValue;
     
-    document.getElementById("total-donate").innerText = inputValue;
+    totalDonate +=inputValue;
+    document.getElementById("total-donate").innerHTML = totalDonate;
 
-
-
-    document.getElementById("money").innerText = donate;
+    document.getElementById("money").innerText = money;
     document.getElementById("noakhali-input").value = "";
     
 });
